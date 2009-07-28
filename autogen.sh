df31f6767f7a8637817199274a0fbecc73bec42c
Alexey Kopytov <akopytov@gmail.com>
2009-07-28 15:41:45 +0400

    * Removed autogenerated ltmain.sh from the repository. * Upgraded autogen.sh to invoke libtoolize to simplify   building from the Bazaar repository. * Updated docs.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-aclocal && automake -c --foreign --add-missing && autoheader && autoconf
+aclocal || exit 1
 
+LIBTOOLIZE=""
+for f in libtoolize glibtoolize ; do
+  if $f --version >/dev/null 2>&1 ; then
+    LIBTOOLIZE=$f
+    break
+  fi
+done
+
+if test -z "$LIBTOOLIZE"; then
+  echo "libtoolize not found, you may want to install libtool on your system"
+  exit 1
+fi
+$LIBTOOLIZE --copy --force || exit 1
+automake -c --foreign --add-missing && autoheader && autoconf
