ea8ebb13daf95284d0551fdeaf526db5da87a997
Vadim Tkachenko <vadim@percona.com>
2011-04-27 15:40:49 -0700

    - Rewrote db scripts to use db_query - combined oltp_complex_ro and oltp_complex_rw into oltp.lua - improved oltp.lua to understand most parameters from sysbench 0.4 oltp

---
+++ sysbench/tests/db/parallel_prepare.lua
@@ -0,0 +1,24 @@
+loadfile("common.lua")()
+
+function thread_init(thread_id)
+   local index_name
+   local i
+   set_vars()
+   
+   print("thread prepare"..thread_id)
+
+   if (oltp_secondary) then
+     index_name = "KEY xid"
+   else
+     index_name = "PRIMARY KEY"
+   end
+
+   for i=thread_id+1, oltp_tables_count, num_threads  do
+   create_insert(i)
+   end
+
+end
+
+function event(thread_id)
+
+end
