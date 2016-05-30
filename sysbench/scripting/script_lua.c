343664e95098c8f3ca963eb4deba725a46f5281d
Espina <espinae@amazon.com>
2016-05-30 11:52:17 -0700

    added locking to get_request in lua scripting

--- sysbench/scripting/script_lua.c
+++ sysbench/scripting/script_lua.c
@@ -241,15 +241,18 @@ sb_request_t sb_lua_get_request(int thread_id)
 
   (void) thread_id; /* unused */
 
+  SB_THREAD_MUTEX_LOCK();
   if (sb_globals.max_requests != 0 && nevents >= sb_globals.max_requests)
   {
     req.type = SB_REQ_TYPE_NULL;
+    SB_THREAD_MUTEX_UNLOCK();
     return req;
   }
 
   req.type = SB_REQ_TYPE_SCRIPT;
   nevents++;
-  
+  SB_THREAD_MUTEX_UNLOCK();
+ 
   return req;
 }
 
