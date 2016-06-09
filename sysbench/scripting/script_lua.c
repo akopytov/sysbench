akopytov/sysbench

commit 9244fcf14ce52fa8d95595f4d5712729229a1b97

Don't lock thread mutex unnecessarily in script_lua.c and sb_cpu.c.
script_lua.c and sb_cpu.c do not actually need to lock any mutexes when
the test duration is not limited by the number of events.

sysbench/scripting/script_lua.c
@@ -241,12 +241,16 @@ sb_request_t sb_lua_get_request(int thread_id)
 
   (void) thread_id; /* unused */
 
-  SB_THREAD_MUTEX_LOCK();
-  if (sb_globals.max_requests != 0 && nevents >= sb_globals.max_requests)
+  if (sb_globals.max_requests > 0)
   {
-    req.type = SB_REQ_TYPE_NULL;
+    SB_THREAD_MUTEX_LOCK();
+    if (nevents >= sb_globals.max_requests)
+    {
+      req.type = SB_REQ_TYPE_NULL;
+      SB_THREAD_MUTEX_UNLOCK();
+      return req;
+    }
     SB_THREAD_MUTEX_UNLOCK();
-    return req;
   }
 
   req.type = SB_REQ_TYPE_SCRIPT;

sysbench/tests/cpu/sb_cpu.c
@@ -70,8 +70,6 @@ static sb_test_t cpu_test =
 static unsigned int    max_prime;
 /* Request counter */
 static unsigned int    req_performed;
-/* Counter mutex */
-static pthread_mutex_t request_mutex;
 
 int register_test_cpu(sb_list_t * tests)
 {
@@ -92,8 +90,6 @@ int cpu_init(void)
 
   req_performed = 0;
 
-  pthread_mutex_init(&request_mutex, NULL);
-  
   return 0;
 }
 
@@ -104,15 +100,20 @@ sb_request_t cpu_get_request(int thread_id)
 
   (void) thread_id; /* unused */
 
-  if (sb_globals.max_requests > 0 && req_performed >= sb_globals.max_requests)
+  if (sb_globals.max_requests > 0)
   {
-    req.type = SB_REQ_TYPE_NULL;
-    return req;
+    SB_THREAD_MUTEX_LOCK();
+    if (req_performed >= sb_globals.max_requests)
+    {
+      req.type = SB_REQ_TYPE_NULL;
+      SB_THREAD_MUTEX_UNLOCK();
+      return req;
+    }
+    req_performed++;
+    SB_THREAD_MUTEX_UNLOCK();
   }
+
   req.type = SB_REQ_TYPE_CPU;
-  pthread_mutex_lock(&request_mutex);
-  req_performed++;
-  pthread_mutex_unlock(&request_mutex);
 
   return req;
 }
@@ -158,7 +159,5 @@ void cpu_print_mode(void)
 
 int cpu_done(void)
 {
-  pthread_mutex_destroy(&request_mutex);
-
   return 0;
 }

commit 3c22365b80ae413b8614b7c904c37aac288fe7f2

Fix crash introduced by commit 9244fcf.
There was a regression introduced by commit regression resulting a
double release of mutex in script_lua.c.

sysbench/scripting/script_lua.c
@@ -250,13 +250,12 @@ sb_request_t sb_lua_get_request(int thread_id)
       SB_THREAD_MUTEX_UNLOCK();
       return req;
     }
+    nevents++;
     SB_THREAD_MUTEX_UNLOCK();
   }
 
   req.type = SB_REQ_TYPE_SCRIPT;
-  nevents++;
-  SB_THREAD_MUTEX_UNLOCK();
- 
+
   return req;
 }
 