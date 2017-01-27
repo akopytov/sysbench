a5b7f22275d534484c17f4c0720ea95f9ec3680a
Alexey Kopytov <akopytov@gmail.com>
2017-01-27 16:05:29 +0300

    Simplify condition in sysbench.lua:thread_run().

--- src/lua/internal/sysbench.lua
+++ src/lua/internal/sysbench.lua
@@ -35,9 +35,9 @@ function thread_run(thread_id)
          local success, ret = pcall(event, thread_id)
 
          if not success then
-            if (type(ret) == "table" and
-                ret.errcode == sysbench.error.RESTART_EVENT) then
-            else
+            if type(ret) != "table" or
+               ret.errcode != sysbench.error.RESTART_EVENT
+            then
                error(ret, 2) -- propagate unknown errors
             end
          end
