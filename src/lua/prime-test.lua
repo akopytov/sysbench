1e0c5b8cd1825faecd7568d0cc36f431c276e211
Alexey Kopytov <akopytov@gmail.com>
2017-06-28 19:20:35 +0300

    Minor fix for prime-test.lua.

--- src/lua/prime-test.lua
+++ src/lua/prime-test.lua
@@ -13,16 +13,17 @@ sysbench.cmdline.options = {
 }
 
 function event()
-    n = 0
+    local n = 0
     for c = 3, sysbench.opt.cpu_max_prime do
-        t = math.sqrt(c)
-        l = 2
+        local t = math.sqrt(c)
+        local isprime = true
         for l = 2, t do
             if c % l == 0 then
+                isprime = false
                 break
             end
         end
-        if l > t then
+        if isprime then
             n = n + 1
         end
     end
