774759b5707c3478df86fc18533deb50e9463752
Alexey Kopytov <akopytov@gmail.com>
2017-10-25 17:17:01 +0300

    Add Ubuntu Artful x86_64/i386 support to buildpack.sh matrix.

--- scripts/buildpack.sh
+++ scripts/buildpack.sh
@@ -56,6 +56,7 @@ distros_x86_64=(
     "ubuntu xenial x86_64"
     "ubuntu yakkety x86_64"
     "ubuntu zesty x86_64"
+    "ubuntu artful x86_64"
     "debian wheezy x86_64"
     "debian jessie x86_64"
 )
@@ -66,6 +67,7 @@ distros_i386=(
     "ubuntu xenial i386"
     "ubuntu yakkety i386"
     "ubuntu zesty i386"
+    "ubuntu artful i386"
     "debian wheezy i386"
     "debian jessie i386"
 )
