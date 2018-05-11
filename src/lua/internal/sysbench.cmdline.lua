aa52c53001cf19da96277a4407f93c37fced7f10
Alexey Kopytov <akopytov@gmail.com>
2018-05-11 22:24:41 +0300

    Update copyright year in sysbench.cmdline.lua.

--- src/lua/internal/sysbench.cmdline.lua
+++ src/lua/internal/sysbench.cmdline.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2017 Alexey Kopytov <akopytov@gmail.com>
+-- Copyright (C) 2017-2018 Alexey Kopytov <akopytov@gmail.com>
 
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
