352e0138eb6141743ed3252cde73db6927e481bd
Alexey Kopytov <akopytov@gmail.com>
2018-06-26 20:15:30 +0300

    Remove Debian Wheezy From CI/build/packaging matrices (EOL).

--- scripts/buildpack.sh
+++ scripts/buildpack.sh
@@ -55,7 +55,6 @@ distros_x86_64=(
     "ubuntu xenial x86_64"
     "ubuntu artful x86_64"
     "ubuntu bionic x86_64"
-    "debian wheezy x86_64"
     "debian jessie x86_64"
     "debian stretch x86_64"
 )
@@ -65,7 +64,6 @@ distros_i386=(
     "ubuntu xenial i386"
     "ubuntu artful i386"
     "ubuntu bionic i386"
-    "debian wheezy i386"
     "debian jessie i386"
     "debian stretch i386"
 )
