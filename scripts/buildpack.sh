5803f73e45c5c9602b5dee749503ef8054bd976c
Alexey Kopytov <akopytov@gmail.com>
2018-12-16 18:52:57 +0300

    build/CI/packaging: remove Fedora 27 (EOL).

--- scripts/buildpack.sh
+++ scripts/buildpack.sh
@@ -49,7 +49,6 @@ PACKPACK_REPO=${PACKPACK_REPO:-akopytov/packpack}
 distros_x86_64=(
     "el 6 x86_64"
     "el 7 x86_64"
-    "fedora 27 x86_64"
     "fedora 28 x86_64"
     "ubuntu trusty x86_64"
     "ubuntu xenial x86_64"
@@ -68,7 +67,6 @@ distros_i386=(
 
 distros_aarch64=(
     "el 7 aarch64"
-    "fedora 27 aarch64"
     "fedora 28 aarch64"
     "ubuntu bionic aarch64"
     "ubuntu trusty aarch64"
