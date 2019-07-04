9df34ccd1a20e6aef0e38e0b8ad52a89cf65dcce
Daniel Black <daniel@linux.ibm.com>
2019-07-04 16:19:09 +1000

    lua return value of sb_rand_varstr

--- src/lua/internal/sysbench.rand.lua
+++ src/lua/internal/sysbench.rand.lua
@@ -32,7 +32,7 @@ uint32_t sb_rand_pareto(uint32_t, uint32_t);
 uint32_t sb_rand_zipfian(uint32_t, uint32_t);
 uint32_t sb_rand_unique(void);
 void sb_rand_str(const char *, char *);
-void sb_rand_varstr(char *, uint32_t, uint32_t);
+uint32_t sb_rand_varstr(char *, uint32_t, uint32_t);
 double sb_rand_uniform_double(void);
 ]]
 
