011e2f5bcc2d11a0ff0e9bc95024b41f819437e3
Tusamarco <Tusamarco@users.noreply.github.com>
2020-03-17 13:16:27 +0100

    change the number iteration form 10 to 100
    just making the script working with 100 instead 10 to make it more descriptive

--- src/lua/test_random.lua
+++ src/lua/test_random.lua
@@ -1,9 +1,9 @@
 function thread_init()
-   h = sysbench.histogram.new(1000, 1, 10)
+   h = sysbench.histogram.new(1000, 1, 100)
 end
 
 function event()
-   h:update(sysbench.rand.default(1, 10))
+   h:update(sysbench.rand.default(1, 100))
 end
 
 function thread_done()
