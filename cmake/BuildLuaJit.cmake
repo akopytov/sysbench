fae8c83ef37eb61bb9042636a5e01bcc190e40a7
Vladislav Vaintroub <vvaintroub@gmail.com>
2023-09-09 09:49:43 +0200

    Windows port - build or find LuaJit library on Windows
    Use instructions from Luajit documentation to build the library.
    Allow FindLuajit.cmake to find luajit if it is built by vcpkg, if system
    luajit library is requested.
    
    Also fix BuildLuajit.cmake to work with Ninja

--- cmake/BuildLuaJit.cmake
+++ cmake/BuildLuaJit.cmake
@@ -1,41 +1,85 @@
 # Build the bundled luajit library and create imported library.
 # luajit::libluajit.
 #
-# This follows the instructions on the luajit page
+# This follows the instructions on the luajit page Both Unix and MSVC build are
+# done in-source, with "make" rsp batch file On *Nix, luajit::libluajit is a
+# static library,on Windows its a  DLL (this has to be so)
 include(ExternalProject)
 
 set(build_dir ${PROJECT_BINARY_DIR}/build/third_party/luajit)
 set(install_dir ${build_dir}/install)
 set(url ${PROJECT_SOURCE_DIR}/third_party/luajit/luajit)
 
-
-# Note : luajit's Makefile is non-portable (GNU)it could fail on BSD, unless
-# gmake is there´
-find_program(MAKE_EXECUTABLE NAMES gmake make REQUIRED)
-# Makefile requires MACOSX_DEPLOYMENT_TARGET on Apple
-if(CMAKE_OSX_DEPLOYMENT_TARGET)
-  set(MACOSX_DEPLOYMENT_TARGET
-      "MACOSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+if(NOT WIN32)
+  # Note : luajit's Makefile is non-portable (GNU)it could fail on BSD, unless
+  # gmake is there´
+  find_program(MAKE_EXECUTABLE NAMES gmake make REQUIRED)
+  # Makefile requires MACOSX_DEPLOYMENT_TARGET on Apple
+  if(CMAKE_OSX_DEPLOYMENT_TARGET)
+    set(MACOSX_DEPLOYMENT_TARGET
+        "MACOSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+  endif()
+  set(libluajit_static_lib ${install_dir}/lib/libluajit-5.1.a)
+  ExternalProject_Add(
+    buildLuaJit
+    URL ${url}
+    SOURCE_DIR ${build_dir}
+    CONFIGURE_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE 1
+    BUILD_ALWAYS FALSE
+    BUILD_COMMAND  ${MAKE_EXECUTABLE} install PREFIX=${install_dir} ${MACOSX_DEPLOYMENT_TARGET}
+    BUILD_BYPRODUCTS ${libluajit_static_lib})
+  add_library(luajit::libluajit STATIC IMPORTED)
+  make_directory(${install_dir}/include/luajit-2.1)
+  set_target_properties(
+    luajit::libluajit
+    PROPERTIES IMPORTED_LOCATION
+               ${libluajit_static_lib}
+               INTERFACE_INCLUDE_DIRECTORIES
+               ${install_dir}/include/luajit-2.1
+               INTERFACE_LINK_LIBRARIES "m;${CMAKE_DL_LIBS}")
+else()
+  if (MINGW)
+    # GNU compatible make required
+    find_program(MAKE_EXECUTABLE NAMES gmake REQUIRED)
+    set(buildCommand ${MAKE_EXECUTABLE})
+    set(luajit_implib ${build_dir}/src/libluajit-5.1.dll.a)
+  else()
+    #MSVC
+    set(buildCommand ${CMAKE_COMMAND} -E chdir src cmd.exe /c msvcbuild.bat debug)
+    set(luajit_implib ${build_dir}/src/lua51.lib)
+  endif()
+  set(luajit_dll ${build_dir}/src/lua51.dll)
+  ExternalProject_Add(
+    buildLuaJit
+    URL ${url}
+    SOURCE_DIR ${build_dir}
+    CONFIGURE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
+    BUILD_COMMAND ${buildCommand}
+    BUILD_BYPRODUCTS ${luajit_implib} ${luajit_dll}
+    BUILD_ALWAYS FALSE)
+  add_library(luajit::libluajit SHARED IMPORTED)
+  set_target_properties(
+    luajit::libluajit
+    PROPERTIES IMPORTED_IMPLIB
+               ${luajit_implib}
+               IMPORTED_LOCATION
+               ${luajit_dll}
+               INTERFACE_INCLUDE_DIRECTORIES
+               ${PROJECT_SOURCE_DIR}/third_party/luajit/luajit/src)
 endif()
-set(lubluajit_static_lib ${install_dir}/lib/libluajit-5.1.a)
-ExternalProject_Add(
-  buildLuaJit
-  URL ${url}
-  SOURCE_DIR ${build_dir}
-  CONFIGURE_COMMAND ""
-  INSTALL_COMMAND ""
-  BUILD_IN_SOURCE 1
-  BUILD_ALWAYS FALSE
-  BUILD_COMMAND  ${MAKE_EXECUTABLE} install PREFIX=${install_dir} ${MACOSX_DEPLOYMENT_TARGET}
-  BUILD_BYPRODUCTS ${libluajit_static_lib})
-add_library(luajit::libluajit STATIC IMPORTED)
-make_directory(${install_dir}/include/luajit-2.1)
-set_target_properties(
-  luajit::libluajit
-  PROPERTIES IMPORTED_LOCATION
-             ${lubluajit_static_lib}
-             INTERFACE_INCLUDE_DIRECTORIES
-             ${install_dir}/include/luajit-2.1
-             INTERFACE_LINK_LIBRARIES "m;${CMAKE_DL_LIBS}")
+
 add_dependencies(luajit::libluajit buildLuaJit)
 
+if(WIN32)
+  # Lua files below are used by sysbench --luajit-cmd
+  # The location must be bin\lua\jit
+  install(
+    DIRECTORY ${build_dir}/src/jit
+    DESTINATION ${CMAKE_INSTALL_BINDIR}/lua
+    USE_SOURCE_PERMISSIONS FILES_MATCHING
+    PATTERN "*.lua")
+endif()
--- cmake/FindLuaJit.cmake
+++ cmake/FindLuaJit.cmake
@@ -19,8 +19,17 @@ FIND_PATH(LUA_INCLUDE_DIR luajit.h
   /opt
 )
 
-
-FIND_LIBRARY(LUA_LIBRARY
+# Test if running on vcpkg toolchain
+if(WIN32 OR (DEFINED VCPKG_TARGET_TRIPLET AND DEFINED VCPKG_APPLOCAL_DEPS))
+  # On vcpkg luajit is 'lua51' and normal lua is 'lua'
+  FIND_LIBRARY(LUA_LIBRARY
+    NAMES lua51
+    HINTS
+    $ENV{LUA_DIR}
+    PATH_SUFFIXES lib
+  )
+else()
+  FIND_LIBRARY(LUA_LIBRARY
     NAMES
     luajit-5.1
     HINTS
@@ -34,20 +43,40 @@ FIND_LIBRARY(LUA_LIBRARY
     /opt/csw
     /opt
   )
+endif()
+
+IF(WIN32)
+  FIND_FILE(LUA_LIBRARY_DLL
+    NAMES lua51.dll
+    PATH_SUFFIXES ../../bin
+  )
+  set(LUA_LIBRARY_DLL_VAR LUA_LIBRARY_DLL)
+else()
+  set(LUA_LIBRARY_DLL_VAR)
+endif()
 
 INCLUDE(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LuaJit_FOUND to TRUE if
 # all listed variables exist
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LuaJit
-  REQUIRED_VARS LUA_LIBRARY LUA_INCLUDE_DIR)
+  REQUIRED_VARS LUA_LIBRARY LUA_INCLUDE_DIR ${LUA_LIBRARY_DLL_VAR})
 
-MARK_AS_ADVANCED(LUA_INCLUDE_DIR LUA_LIBRARY)
+MARK_AS_ADVANCED(LUA_INCLUDE_DIR LUA_LIBRARY ${LUA_LIBRARY_DLL_VAR})
 
 if (LuaJit_FOUND AND NOT TARGET luajit::libluajit)
-  add_library(luajit::libluajit UNKNOWN IMPORTED)
-  set_target_properties(luajit::libluajit PROPERTIES
-    IMPORTED_LOCATION "${LUA_LIBRARY}"
-    INTERFACE_LINK_LIBRARIES "m;${CMAKE_DL_LIBS}" # Linuxism?
+  if(WIN32)
+    add_library(luajit::libluajit SHARED IMPORTED)
+    get_filename_component(LUA_LIBRARY_DLL "${LUA_LIBRARY_DLL}" REALPATH)
+    set_target_properties(luajit::libluajit PROPERTIES
+      IMPORTED_LOCATION "${LUA_LIBRARY_DLL}"
+      IMPORTED_IMPLIB "${LUA_LIBRARY}")
+  else()
+    add_library(luajit::libluajit UNKNOWN IMPORTED)
+    set_target_properties(luajit::libluajit PROPERTIES
+      IMPORTED_LOCATION "${LUA_LIBRARY}"
+      INTERFACE_LINK_LIBRARIES "m;${CMAKE_DL_LIBS}" # Linuxism?
+    )
+  endif()
   set_target_properties(luajit::libluajit PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${LUA_INCLUDE_DIR}")
 endif()
